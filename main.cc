3644913053017b21d3bc68c02e94186453c6c87b
Baturay <baturayturan@gmail.com>
2022-11-15 20:29:16 +0100

    Working on main

--- main.cc
+++ main.cc
@@ -1,44 +1,95 @@
- 
+#include <iostream>
 #include <gf/Event.h>
 #include <gf/Font.h>
 #include <gf/RenderWindow.h>
 #include <gf/Sprite.h>
 #include <gf/Text.h>
 #include <gf/Window.h>
+#include <gf/Entity.h>
+#include <gf/Shapes.h>
+
  
-int main() {
-  // Create the main window and the renderer
- 
-  gf::Window window("Example", { 640, 480 });
-  gf::RenderWindow renderer(window);
- 
- 
-  renderer.clear(gf::Color::White);
- 
-  // Start the game loop
- 
-  while (window.isOpen()) {
- 
-    // Process events
- 
-    gf::Event event;
- 
-    while (window.pollEvent(event)) {
-      switch (event.type) {
-        case gf::EventType::Closed:
-          window.close();
-          break;
- 
-        default:
-          break;
-      }
+class Player : public gf::Entity{
+    public:
+    void onKeyPress(gf::Event e){
+        std::cout << "Key press ! \n";
+        switch (e.key.keycode){
+            case gf::Keycode::Right:
+                this->m_velocity = {5,0};
+                break;
+            case gf::Keycode::Left:
+                this->m_velocity = {-5,0};
+                break;     
+        }
     }
+    virtual void update(float dt){
+        this->m_position += this->m_velocity  * this->speed * dt;
+    }
+    virtual void render(gf::RenderTarget& target){
+        gf::RectangleShape shape({ 20,20 });
+        shape.setPosition(m_position);
+        shape.setColor(gf::Color::Black);
+        shape.setAnchor(gf::Anchor::Center);
+        target.draw(shape);
+    };
+    private:
+    gf::Vector2f m_position; // center of the square
+    gf::Vector2f m_velocity;
+    double speed;
+
+};
+
+class Game{
+    public:    
+    Game() : window("My awesome game", { 640, 480 }),renderer(window){        
+        this->player = Player();
+    }
+
+    void gameloop(){
+        this->renderer.clear(gf::Color::White);
+        gf::Clock clock;
+        while (this->window.isOpen()) {
+    
+            // Process events
+        
+            gf::Event event;
+        
+            while (this->window.pollEvent(event)) {
+                switch (event.type) {
+                    case gf::EventType::Closed:
+                        this->window.close();
+                    break;
+                    case gf::EventType::KeyPressed:
+                        this->player.onKeyPress(event);
+                        break;
+                    default:
+                    break;
+                }
+            }
+        
+            //Update
+            float dt = clock.restart().asSeconds();
+            this->player.update(dt);
+            // Draw the entities
+        
+            this->renderer.clear();
+            this->player.render(this->renderer);
+            this->renderer.display();
+        }
+    }
+    private: 
+    Player player;
+    gf::Window window;
+    gf::RenderWindow renderer;
+    
+};
+
+int main() {
+
  
-    // Draw the entities
  
-    renderer.clear();
-    renderer.display();
-  }
+  Game game;
+  game.gameloop();
  
   return 0;
 }
