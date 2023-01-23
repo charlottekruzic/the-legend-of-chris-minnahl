83ab3fd8e30bc48216f038091280d9d548ec507e
Baturay <baturayturan@gmail.com>
2023-01-23 08:32:14 +0100

    re-added level + collisions

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define MANAGER_DATA_DIR "/usr/local"
+#define MANAGER_DATA_DIR "/usr/local/share/games/Commando"
 
 #endif // CONFIG_H
--- local/level.h
+++ local/level.h
@@ -6,84 +6,34 @@
 #include <gf/RenderWindow.h>
 #include <gf/Text.h>
 #include <gf/Font.h>
-#include <string>
+#include <string.h>
 #include "wall.h"
 #include "player.h"
 #include "guard.h"
 
-class Level{
-    private:
-        std::vector<std::vector<Wall>> map;
-        gf::Vector2i start,end,object,statue;
-        int width,height;
-        Player* player;
-        bool win, loose;
-        std::vector<Guard*> guards;
-        gf::RectangleShape background;
-        std::string level_path;
-        int numberTotalOfObject=0;
-    public:
-        Level(Player* player,std::string path);
-        /**
-         * @brief Adds a wall to the level at the given position
-         * @param position grid position of the wall
-         * @return true if a wall was created
-        */
+class Level : public gf::Entity{
+	private:
+		std::vector<std::vector<Wall>> map;
+		std::vector<Wall> objects;
+		std::vector<Wall> statues;
 
-        
-        bool addWall(gf::Vector2i position);
-
-		/**
-		*@brief Load a level from a text file.
-		*@param path to the text file from root directory
-		*/
+		Wall start;
+		Wall end;
+		
+		Player& player;
+		bool isGameOver;
+		
+		
+	public:
+		Level(Player & player);
+		void init();
 		void load(std::string path);
+		virtual void render(gf::RenderTarget & target,
+				const gf::RenderStates & states);	
+		void addWall(gf::Vector2i position,WallType type);	
+		void update(gf::Time time);
+		gf::RectF findCollider();
+		bool checkGameOver();
 
-        gf::Vector2f getSize();
-
-        float getWidth();
-
-        float getHeight();
-
-		Guard * addGuard(gf::Vector2i grid_pos,std::vector<RouteAction *> route);
-
-        void reset();
-
-        bool isWin();
-
-        bool isLoose();
-
-
-        bool removeWall(gf::Vector2i position);
-
-        void prettyPrint();
-
-        Wall* checkCollisions();
-
-        bool checkWin();
-
-        bool checkLoose();
-
-
-        void checkTakeObject();
-
-        void checkStatue();
-
-        bool isFreeSpace(gf::Vector2i position);
-
-        bool setStart(gf::Vector2i position);
-
-        bool setEnd(gf::Vector2i position);
-
-        bool setObject(gf::Vector2i pos);
-
-        bool setStatue(gf::Vector2i pos);
-
-        void update(float dt);
-
-        void render(gf::RenderTarget& target, bool isMinimap);
-
-        void renderScore(gf::RenderTarget& target, gf::Vector2f size);
-
-};
+};		
 #endif
